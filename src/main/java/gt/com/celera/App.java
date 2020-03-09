2d678e5a4a0d936934a0283436a16dd80e779f24
RobertoC27 <chiroy_134@hotmail.com>
2020-03-09 17:16:45 -0600

    list all queues

--- src/main/java/gt/com/celera/App.java
+++ src/main/java/gt/com/celera/App.java
@@ -1,13 +1,23 @@
 package gt.com.celera;
 
+import software.amazon.awssdk.services.sqs.SqsClient;
+import software.amazon.awssdk.services.sqs.model.ListQueuesRequest;
+import software.amazon.awssdk.services.sqs.model.ListQueuesResponse;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        SqsClient sqsClient = SqsClient.builder()
+                .build();
+        System.out.println("Hello World!");
+        String prefix = "que";
+        ListQueuesRequest listQueuesRequest = ListQueuesRequest.builder().build();
+        ListQueuesResponse listQueuesResponse = sqsClient.listQueues(listQueuesRequest);
+        for (String url : listQueuesResponse.queueUrls()) {
+            System.out.println(url);
+        }
     }
 }
