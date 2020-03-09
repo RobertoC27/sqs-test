e674f18479e384416f46dda214b497e782d8cf8f
RobertoC27 <chiroy_134@hotmail.com>
2020-03-09 16:39:12 -0600

    initial commit

---
+++ src/main/java/gt/com/celera/App.java
@@ -0,0 +1,13 @@
+package gt.com.celera;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
